c3d706e74bc5fb1b7892c66dfad84f06641abeb5
Matheus Concon <142353640+goldjkk@users.noreply.github.com>
2023-10-26 21:21:08 -0300

    Create proj.h
    criando proj.h

---
+++ proj.h
@@ -0,0 +1,34 @@
+//
+// Created by unifmatholiveira on 27/09/2023.
+//
+
+#ifndef UNTITLED1_PROJ_H
+#define UNTITLED1_PROJ_H
+#ifndef PROJ_H
+#define PROJ_H
+
+typedef struct {
+    char nome[50];
+    char cpf[12];
+    char tipo_conta[10];
+    float saldo;
+    char senha[20];
+} Cliente;
+
+typedef struct {
+    char cpf[12];
+    char tipo_operacao[20];
+    float valor;
+} Operacao;
+
+void Novo_Cliente(Cliente clientes[], int *numClientes);
+void Apagar_Cliente(Cliente clientes[], int *numClientes);
+void Listar_clientes(Cliente clientes[], int numClientes);
+void Debito(Cliente clientes[], int numClientes, Operacao operacoes[], int *numOperacoes);
+void Deposito(Cliente clientes[], int numClientes, Operacao operacoes[], int *numOperacoes);
+void Extrato(Cliente clientes[], int numClientes, Operacao operacoes[], int numOperacoes);
+void Transferencia(Cliente clientes[], int numClientes, Operacao operacoes[], int *numOperacoes);
+
+#endif
+
+#endif //UNTITLED1_PROJ_H
