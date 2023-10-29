491219187dc8877afaaf64839228e28d0cdd0262
Kaue010101 <149123288+Kaue010101@users.noreply.github.com>
2023-10-29 15:27:23 -0300

    Update proj.h

--- proj.h
+++ proj.h
@@ -1,12 +1,10 @@
-//
-// Created by unifmatholiveira on 27/09/2023.
-//
 
 #ifndef UNTITLED1_PROJ_H
 #define UNTITLED1_PROJ_H
 #ifndef PROJ_H
 #define PROJ_H
 
+// Definição da estrutura Cliente
 typedef struct {
     char nome[50];
     char cpf[12];
@@ -14,20 +12,14 @@ typedef struct {
     float saldo;
     char senha[20];
 } Cliente;
-
+// Definição da estrutura Operacao
 typedef struct {
     char cpf[12];
     char tipo_operacao[20];
     float valor;
 } Operacao;
 
-void Novo_Cliente(Cliente clientes[], int *numClientes);
 void Apagar_Cliente(Cliente clientes[], int *numClientes);
-void Listar_clientes(Cliente clientes[], int numClientes);
-void Debito(Cliente clientes[], int numClientes, Operacao operacoes[], int *numOperacoes);
-void Deposito(Cliente clientes[], int numClientes, Operacao operacoes[], int *numOperacoes);
-void Extrato(Cliente clientes[], int numClientes, Operacao operacoes[], int numOperacoes);
-void Transferencia(Cliente clientes[], int numClientes, Operacao operacoes[], int *numOperacoes);
 
 #endif
 
