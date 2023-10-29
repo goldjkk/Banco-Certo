619ca899938ec6c29167d96036576e07a3804e50
Kaue010101 <149123288+Kaue010101@users.noreply.github.com>
2023-10-29 15:24:58 -0300

    Update proj.h

--- proj.h
+++ proj.h
@@ -1,6 +1,3 @@
-//
-// Created by unifmatholiveira on 27/09/2023.
-//
 
 #ifndef UNTITLED1_PROJ_H
 #define UNTITLED1_PROJ_H
@@ -22,13 +19,6 @@ typedef struct {
 } Operacao;
 
 void Novo_Cliente(Cliente clientes[], int *numClientes);
-void Apagar_Cliente(Cliente clientes[], int *numClientes);
-void Listar_clientes(Cliente clientes[], int numClientes);
-void Debito(Cliente clientes[], int numClientes, Operacao operacoes[], int *numOperacoes);
-void Deposito(Cliente clientes[], int numClientes, Operacao operacoes[], int *numOperacoes);
-void Extrato(Cliente clientes[], int numClientes, Operacao operacoes[], int numOperacoes);
-void Transferencia(Cliente clientes[], int numClientes, Operacao operacoes[], int *numOperacoes);
-
 #endif
 
 #endif //UNTITLED1_PROJ_H
