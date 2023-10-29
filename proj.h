6a0cc1e2bbeb5619383545157618792533ee02d6
Matheus Concon <142353640+goldjkk@users.noreply.github.com>
2023-10-29 15:24:27 -0300

    Update proj.h

--- proj.h
+++ proj.h
@@ -6,7 +6,7 @@
 #define UNTITLED1_PROJ_H
 #ifndef PROJ_H
 #define PROJ_H
-
+// Definição da estrutura Cliente
 typedef struct {
     char nome[50];
     char cpf[12];
@@ -14,19 +14,13 @@ typedef struct {
     float saldo;
     char senha[20];
 } Cliente;
-
+// Definição da estrutura Operacao
 typedef struct {
     char cpf[12];
     char tipo_operacao[20];
     float valor;
 } Operacao;
 
-void Novo_Cliente(Cliente clientes[], int *numClientes);
-void Apagar_Cliente(Cliente clientes[], int *numClientes);
-void Listar_clientes(Cliente clientes[], int numClientes);
-void Debito(Cliente clientes[], int numClientes, Operacao operacoes[], int *numOperacoes);
-void Deposito(Cliente clientes[], int numClientes, Operacao operacoes[], int *numOperacoes);
-void Extrato(Cliente clientes[], int numClientes, Operacao operacoes[], int numOperacoes);
 void Transferencia(Cliente clientes[], int numClientes, Operacao operacoes[], int *numOperacoes);
 
 #endif
